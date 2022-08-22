a7951196535e4b11c369847d2c34c3e1dc6e6e8d
geoduf <37289278+geoduf@users.noreply.github.com>
2022-08-22 12:10:59 +0200

    refactor: use 'v2+' module strategy

--- v2/number_range.go
+++ v2/number_range.go
@@ -2,9 +2,9 @@ package grange
 
 import "fmt"
 
-type RangeNumber[K Number] Range[K]
+type NumberRange[K Number] Range[K]
 
-func (r RangeNumber[K]) ToPostgresString() (string, string, string) {
+func (r NumberRange[K]) ToPostgresString() (string, string, string) {
 	start := r[0]
 	end := r[1]
 	startBoundString := "["
@@ -19,17 +19,17 @@ func (r RangeNumber[K]) ToPostgresString() (string, string, string) {
 	return fmt.Sprintf("%v", start.Value), fmt.Sprintf("%v", end.Value), startBoundString + endBoundString
 }
 
-func (r RangeNumber[K]) Intersection(r1 RangeNumber[K]) *RangeNumber[K] {
+func (r NumberRange[K]) Intersection(r1 NumberRange[K]) *NumberRange[K] {
 	return intersection(r, r1)
 }
 
-func (r RangeNumber[K]) IsEmpty() bool {
+func (r NumberRange[K]) IsEmpty() bool {
 	return r[0].Value == r[1].Value && (r[0].IsExclusive || r[1].IsExclusive)
 }
 
 // if start and end bound have same value and one of the value is exclusive
 // so the range is empty
-func formatEmptyExclusiveRange[K Number](r RangeNumber[K]) *RangeNumber[K] {
+func formatEmptyExclusiveRange[K Number](r NumberRange[K]) *NumberRange[K] {
 	if r.IsEmpty() {
 		return nil
 	}
